ec66478ac923c0c9ebf1146ed852c60e804fdadc
Will Gregory <William.gregory@outlook.com>
2024-07-26 14:36:54 -0400

    Update ENKF_mpi.py

--- online_iceocean/ENKF_mpi.py
+++ online_iceocean/ENKF_mpi.py
@@ -168,7 +168,7 @@ y,m,d = np.genfromtxt('coupler.res',skip_header=1)[1,:3].astype(np.int32)
 date = datetime(y,m,d).strftime('%Y%m%d')
 
 ### FILES ###                                                                                                                                                                                                                            
-obs_file = '/gpfs/f5/gfdl_o/scratch/William.Gregory/NTSIC/regrid_OM4/NSIDC0051_SEAICE_SPEAR1deg_'+date+'_v2.0.nc'
+obs_file = '/gpfs/f5/gfdl_o/scratch/'+user+'/NTSIC/regrid_OM4/NSIDC0051_SEAICE_SPEAR1deg_'+date+'_v2.0.nc'
 if os.path.exists(obs_file):
     grid = xr.open_dataset('/ncrc/home2/William.Gregory/dart_manhattan/ice.static.nc')
     lon = grid.GEOLON.to_numpy()
